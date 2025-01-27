7337f10040741789c8d75de7e487ee6147ba32ff
Ivan Shpynda <ivanshpynda@waterford.org>
2025-01-27 20:34:56 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# React + Vite
+# Heritage
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+When I think about my heritage, family stories and old yellowed photographs immediately come to mind. In one of them, my great-grandfather Stepan is dressed in the military uniform of the Austro-Hungarian Empire. From my older relatives, I have often heard semi-legendary tales about him. For instance, how at the age of sixteen, he crossed the ocean to the United States in search of work. Or how during the war, he refused to shoot at the enemy, as he was deeply religious and did not want to be complicit in killing.
 
-Currently, two official plugins are available:
+His son, my grandfather Ivan, studied at the Greek-Catholic seminary in Przemyśl and later became a priest. In 1946, the church was abolished, and he, along with his family, was deported from Lemkivshchyna to Ternopil Oblast. This was a tragic event, but without it, perhaps I would not exist. After all, it was because of this resettlement that he married my grandmother, who had also been deported from her native lands.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Thinking about this, I feel the pages of history textbooks come alive, gaining depth and emotion. Abstract concepts like "resettlement" or "war" cease to be mere dry facts—they become personal stories, intertwined with my own life. I realize that behind every great historical event are the fates of ordinary people, like my great-grandfather or grandfather.
+
+My heritage is not just a memory of the past. It is a living bridge between generations, passing on values, traditions, and experiences. These are the stories that shape my identity and help me better understand myself. This awareness brings me a sense of connection to something greater—to my family, to history, to the culture of my people. It also makes me reflect on the legacy I will leave behind. What stories will my descendants tell? Ultimately, heritage is not only about what has been but also about what we create for the future. And this adds a special meaning to each of my days.
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <header>
+                <img src="../public/logo.jpg" alt="logo" className="logo-img" />
+                <p className="header-text">ГКУ-24М</p>
+            </header>
+            <div className="container">
+                <h1>Спадщина, памʼять, ідентичність </h1>
+                <div className="carousel-wrapper">
+                    <Carousel infiniteLoop={true}>
+                        <div>
+                            <img src="../public/photo-1.jpg" alt="image-1" />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                            <p className="legend">
+                                Степан Шпинда під час Першої світової війни
+                            </p>
+                        </div>
+                        {/* <div>
+                        <img src="../public/photo-2.jpg" alt="image-1" />
+                        <p className="legend">Родинне весілля на Лемківщині</p>
+                    </div> */}
+                        <div>
+                            <img src="../public/photo-3.jpg" alt="image-1" />
+                            <p className="legend">
+                                Марися Шипак з родиною, Лемківщина
+                            </p>
+                        </div>
+                        <div>
+                            <img src="../public/photo-4.jpg" alt="image-1" />
+                            <p className="legend">
+                                Степан та Іван Шпинди у Перемишлі, 1938 рік
+                            </p>
+                        </div>
+                        <div>
+                            <img src="../public/photo-5.jpg" alt="image-1" />
+                            <p className="legend">
+                                Іван та Марися Шпинди у Підволочиську, 1946 рік
+                            </p>
+                        </div>
+                    </Carousel>
+                </div>
+            </div>
+            <footer>
+                <p className="footer-text">
+                    &copy; {new Date().getFullYear()}, developed by Ivan Shpynda
+                </p>
+            </footer>
+        </>
+    );
 }
 
-export default App
+export default App;
